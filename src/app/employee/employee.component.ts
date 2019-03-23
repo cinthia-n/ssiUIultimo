9e6e001a873dd62c0631a3c017fa865b720abbf3
IsaiaX <isaiastufatherx@gmail.com>
2019-03-23 19:09:15 -0400

    delete funcionando, de forma manual insertar el ID

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -7,6 +7,8 @@ import {RegEmployeeComponent} from '../reg-employee/reg-employee.component';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {RequestService} from '../services/request.service';
 import {AppURL} from '../shared/appUrl';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-employee',
@@ -23,7 +25,9 @@ export class EmployeeComponent implements OnInit {
   employees: Employee[];
   user: FormGroup;
   public url = AppURL.getUrlEmployee();
-  constructor(private employeeService: EmployeeService, public dialog: MatDialog, private requestService: RequestService) {
+  private baseUrl2 = '';
+
+  constructor(private http: HttpClient, private employeeService: EmployeeService, public dialog: MatDialog, private requestService: RequestService) {
   }
   // "firstName": "John kintaro",
   // "lastName": "Doe",
@@ -57,17 +61,52 @@ export class EmployeeComponent implements OnInit {
     console.log( this.user.value);
   }
 
-  delete() {
-    this.requestService.add(this.url, this.user.value, []).subscribe(
+  onBorrar2(id: number): Observable<any> {
+    console.log('vamos ');
+    console.log(this.url+'/'+id);
+
+    // console.log(this.requestService.delete(this.url, id));
+    // return this.http.delete(`${this.baseUrl2}/${id}`, { responseType: 'text' });
+    // return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+    return this.requestService.delete(this.url, id);
+  }
+
+  /*deleteUser(user){ this.requestService.delete(this.url, '3').subscribe(
+      (data)=>{
+      this.user.splice(this.user.indexOf(user), 1);
+    },(error)=>{
+      console.log(error);
+    });
+  }*/
+  /*borrar(archivoParaBorrar) {
+    const url = `http://localhost/api/public/delete/${archivoParaBorrar.user_id}`;
+
+    this._http.delete(url).subscribe(data => {
+      console.log('se ha borrado correctamente');
+      let index = archivo.indexOf(archivoParaBorrar, 0);
+      if (index > -1) {
+        archivo.splice(index, 1); // Tu array de archivos
+      }
+    }, error => {
+      console.log('error');
+    });
+  }*/
+
+  onBorrar() {
+    this.requestService.delete(this.url, 1).subscribe(
       response => {
+        // this.router.navigate(['../'], { relativeTo: this.route });
+        // this.successDeleted(res);
         //  this.user.reset();
         //  this.router.navigate(['/compra']);
       },
       error => {
+        console.log('hay errores muchcachos');
         console.log(error);
       }
     );
     console.log( this.user.value);
+    // console.log('ivancete delete');
   }
   openRegEmployee() {
     this.dialog.open(RegEmployeeComponent, {width: '500px', height: '450px'});
