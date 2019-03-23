71062b03e5e06aba3f8987a3618545b4c45069f6
IsaiaX <isaiastufatherx@gmail.com>
2019-03-22 21:24:55 -0400

    doctor 777 serafo insert employee en la BD

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -5,6 +5,8 @@ import {LoginComponent} from '../login/login.component';
 import {MatDialog} from '@angular/material';
 import {RegEmployeeComponent} from '../reg-employee/reg-employee.component';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {RequestService} from '../services/request.service';
+import {AppURL} from '../shared/appUrl';
 
 @Component({
   selector: 'app-employee',
@@ -20,17 +22,51 @@ export class EmployeeComponent implements OnInit {
 
   employees: Employee[];
   user: FormGroup;
-  constructor(private employeeService: EmployeeService, public dialog: MatDialog) {
+  public url = AppURL.getUrlEmployee();
+  constructor(private employeeService: EmployeeService, public dialog: MatDialog, private requestService: RequestService) {
   }
+  // "firstName": "John kintaro",
+  // "lastName": "Doe",
+  // "image": null,
+  // "jobPosition": null,
+  // "jobCode": null,
+  // "featured": true,
+  // "jobDescription": "Descripcion de job"
   ngOnInit() {
     this.employeeService.getEmployees().subscribe(employees => this.employees = employees);
     this.user = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-      password: new FormControl('', Validators.required),
-      passwordRepeat: new FormControl('', Validators.required)
+      firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      lastName: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required),
+      jobPosition: new FormControl('', Validators.required),
+      jobCode: new FormControl('', Validators.required),
+      featured: new FormControl('', Validators.required),
+      jobDescription: new FormControl('', Validators.required)
     });
   }
   onSubmit() {
+    this.requestService.add(this.url, this.user.value, []).subscribe(
+      response => {
+      //  this.user.reset();
+        //  this.router.navigate(['/compra']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    console.log( this.user.value);
+  }
+
+  delete() {
+    this.requestService.add(this.url, this.user.value, []).subscribe(
+      response => {
+        //  this.user.reset();
+        //  this.router.navigate(['/compra']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
     console.log( this.user.value);
   }
   openRegEmployee() {
